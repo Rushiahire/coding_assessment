a3d7d2e3ab642b533bbaa6383d9bfa78a61cb296
Rushiahire <rushiahire9567@gmail.com>
2022-08-09 17:04:48 +0530

    Navbar with routes done

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes,Route } from 'react-router-dom';
+import LoginPage from './components/Login/LoginPage';
+import Home from './components/Home/Home';
+import ListingPage from './components/Listing/ListingPage';
+import FormPage from './components/Form/FormPage';
+
+
+
+function AppRoutes() {
+  return (
+    <>
+    <Routes>
+        <Route path={"/"}  element={<Home/>} />
+        <Route path={"/login"}  element={<LoginPage/>} />
+        <Route path={"/Form"}  element={<FormPage/>} />
+        <Route path={"/List"}  element={<ListingPage/>} />
+
+    </Routes>
+    </>
+  )
+}
+
+export default AppRoutes
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      {/* <div classNameName="container"> */}
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid ">
+            <Nav.Link as={Link} to={"/"} className="navbar-brand ">
+              Navbar
+            </Nav.Link>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Nav.Link as={Link} to={"/"}>
+                    Home
+                  </Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link as={Link} to={"/login"}>
+                    Login
+                  </Nav.Link>
+                </li>
+
+                <li className="nav-item">
+                  <Nav.Link as={Link} to={"/form"}>
+                    Form
+                  </Nav.Link>
+                </li>
+
+                <li className="nav-item">
+                  <Nav.Link as={Link} to={"/form"}>
+                    Form
+                  </Nav.Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      {/* </div> */}
+    </>
+  );
+};
+
+export default Navbar;
